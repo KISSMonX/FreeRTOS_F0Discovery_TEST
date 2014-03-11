d8b7a6cb55a5f9d82703fac5d062258cdbedd6cf
KISSMonX <omonkerman@foxmail.com>
2014-03-11 17:22:31 +0800

    增加 RTE 文件夹的内容

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32F0_Discovery_TEST' 
+ * Target:  'STM32F030R8T6' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STARTUP_STM32F0xx    /* Device Startup for STM32F0 */
+
+#endif /* RTE_COMPONENTS_H */
